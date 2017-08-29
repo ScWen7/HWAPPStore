2379f36e15e66c3590fdecbae5e050aaa09880ee
ScWen7 <ab811733738@163.com>
2017-08-29 10:47:19 +0800

    1、首页tablayout+viewpager联动 2、沉浸式状态栏及tablayout切换效果

--- app/src/main/java/io/scwen7/hwappstore/ui/activity/MainActivity.java
+++ app/src/main/java/io/scwen7/hwappstore/ui/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package io.scwen7.hwappstore.ui.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -11,12 +12,14 @@ import java.util.List;
 import butterknife.BindView;
 import io.scwen7.hwappstore.R;
 import io.scwen7.hwappstore.base.BaseActivity;
+import io.scwen7.hwappstore.common.utils.UIUtils;
 import io.scwen7.hwappstore.ui.adapter.MainPagerAdapter;
 import io.scwen7.hwappstore.ui.fragment.CatrgoryFragment;
 import io.scwen7.hwappstore.ui.fragment.ManageFragment;
 import io.scwen7.hwappstore.ui.fragment.RankFragment;
 import io.scwen7.hwappstore.ui.fragment.RecoFragment;
 import io.scwen7.hwappstore.ui.fragment.UserFragment;
+import io.scwen7.hwappstore.weight.StatusBarUtil;
 
 public class MainActivity extends BaseActivity {
 
@@ -37,11 +40,40 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initData(Bundle savedInstanceState) {
 
+        StatusBarUtil.setTranslucentForImageView(this, 0, mTabMain);
+
+
         initFragments();
 
         MainPagerAdapter mainPagerAdapter = new MainPagerAdapter(getSupportFragmentManager(), mFragments, titles);
         mViewpagerMain.setAdapter(mainPagerAdapter);
         mTabMain.setupWithViewPager(mViewpagerMain);
+
+
+        mViewpagerMain.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0) {
+                    mTabMain.setBackgroundColor(Color.TRANSPARENT);
+                    mTabMain.setSelectedTabIndicatorColor(Color.WHITE);
+                    mTabMain.setTabTextColors(Color.WHITE, Color.WHITE);
+                } else {
+                    mTabMain.setBackgroundColor(Color.WHITE);
+                    mTabMain.setSelectedTabIndicatorColor(UIUtils.getColor(R.color.colorPrimaryDark));
+                    mTabMain.setTabTextColors(UIUtils.getColor(R.color.textgray), UIUtils.getColor(R.color.colorPrimaryDark));
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
     }
 
@@ -53,6 +85,7 @@ public class MainActivity extends BaseActivity {
         mFragments.add(new ManageFragment());
         mFragments.add(new UserFragment());
     }
+
 
 }
 
